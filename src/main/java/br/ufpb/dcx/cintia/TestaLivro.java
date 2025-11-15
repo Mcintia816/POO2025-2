8b5ea9bc99782784a2ad5bfb531980eea9330cfb
Mcintia816 <cintia.alcari@dcx.ufpb.br>
2025-11-14 23:32:34 -0300

    Refactor TestaLivro to use JOptionPane for input
    Updated TestaLivro to allow user input for book details.

--- src/main/java/br/ufpb/dcx/cintia/TestaLivro.java
+++ src/main/java/br/ufpb/dcx/cintia/TestaLivro.java
@@ -1,19 +1,36 @@
 package br.ufpb.dcx.cintia;
 
- public class TestaLivro {
-    public static void main(String[] args){
-        SistemaLivro livro = new SistemaLivro();
+
+import javax.swing.*;
 
 
-        livro.Buscar("");
+public class TestaLivro {
+   public static void main(String[] args) throws Exception {
+       SistemaLivro livro = new SistemaLivro();
+       int quant = Integer.parseInt(JOptionPane.showInputDialog("Quantos Livros livros voce deseja colocar na lista?"));
+       for (int i = 0; i < quant; i++) {
+           String autor = JOptionPane.showInputDialog("Nome do Autor: ");
+           String titulo = JOptionPane.showInputDialog("Titulo do livro: ");
+           String genero = JOptionPane.showInputDialog("Genero: ");
+           int quantPaginas = Integer.parseInt(JOptionPane.showInputDialog("Quantidade de páginas: "));
 
-        try {
-            Livro l = new Livro("");
-            livro.AdicionarLivro(l);
-            } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    }
+           Livro livro1 = new Livro(autor,titulo,genero,quantPaginas);
+           livro.AdicionarLivro(livro1);
 
+
+       }
+       for (Livro l : livro.getLivros()) {
+           System.out.println(l.toString());
+
+
+
+
+       }
+      
+
+
+   }
+
+
+}
